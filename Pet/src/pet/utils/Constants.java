21df0ab440850b3978518c2a1f3e4dcee886d6b7
nguyenhuuca <nguyenhuuca@gmail.com>
2018-05-06 19:26:19 +0700

    adding doc

--- Pet/src/pet/utils/Constants.java
+++ Pet/src/pet/utils/Constants.java
@@ -11,6 +11,6 @@ public class Constants {
   /** The Constant FORMAT_STR. */
   public static final String FORMAT_STR = "%-50s %-15s %-15s %-15s %-15s %-15s %-15s %-15s %-15s";
   
-  /** The header. */
-  public static String HEADER = String.format(Constants.FORMAT_STR,"CLUB","MP","W","D", "L", "GF","GA", "GD", "PTS");
+  /** The Constant header. */
+  public static final String HEADER = String.format(Constants.FORMAT_STR,"CLUB","MP","W","D", "L", "GF","GA", "GD", "PTS");
 }
